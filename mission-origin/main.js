ea949d8954a874f2bf17ef5d318db3a6a1a1e66c
André Quintino <pandrequintino@gmail.com>
2022-05-06 09:32:49 +0100

    mission origin: stage 4

--- mission-origin/main.js
+++ mission-origin/main.js
@@ -1,7 +1,20 @@
+window.addEventListener('scroll', onScroll);
+
 function onScroll() {
+	showNavOnScroll();
+	showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
 	if (scrollY > 0) return navigation.classList.add('scroll');
 
 	return navigation.classList.remove('scroll');
+}
+
+function showBackToTopButtonOnScroll() {
+	if (scrollY > 550) return backToTopButton.classList.add('show');
+
+	return backToTopButton.classList.remove('show');
 }
 
 function openMenu() {
